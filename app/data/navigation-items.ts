b72b82f1cade1ad710e6895b6c3509f66bc050ef
Nxlogix <165151602+Nxlogix@users.noreply.github.com>
2026-09-23 11:18:33 -0600

    Práctica 11: Mejora del componente Header y optimización de navegación

---
+++ app/data/navigation-items.ts
@@ -0,0 +1,16 @@
+import { NavigationItem } from "../types/navigation";
+
+export const navigationItems = [
+  {
+    href: "/",
+    label: "Mi carrera",
+  },
+  {
+    href: "/about",
+    label: "Acerca de",
+  },
+  {
+    href: "/blog",
+    label: "Blog",
+  },
+] satisfies NavigationItem[];
